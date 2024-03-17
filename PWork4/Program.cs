13d95ebb883d1c31fb494c21a5f4ae112b7dab6d
Andrii99H <andrii99havryliuk@gmail.com>
2024-03-17 13:55:58 +0200

    Wrote input and some func

--- PWork4/Program.cs
+++ PWork4/Program.cs
@@ -4,10 +4,35 @@
     {
         static void Main(string[] args)
         {
+            string inputLine;
+
+            Console.WriteLine("Hello! It`s a string roulette");
+
+            while (true)
+            {
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.Write("Enter text: ");
+                inputLine = Console.ReadLine();
+                if (string.IsNullOrEmpty(inputLine))
+                {
+                    Console.WriteLine("Wrong input! Try again? (y - yes/other сharacter(s) - no)");
+                    if (Console.ReadLine() == "y")
+                        continue;
+                    else break;
+                }
             
+                Console.WriteLine($"\nText \"{inputLine}\" include: {inputLine.Length} symbol(s)");
+
+                Console.WriteLine("Try again? (y - yes/other сharacter(s) - no)");
+                if (Console.ReadLine() == "y")
+                    continue;
+                else break;
+
+            }
 
 
-
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("Goodbye!");
             Console.ReadKey();
         }
     }
