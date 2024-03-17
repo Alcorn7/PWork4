563d91bcf98a6b05d831965e8c3367bd929e49cb
Andrii99H <andrii99havryliuk@gmail.com>
2024-03-17 14:09:54 +0200

    Add ASCII table

--- PWork4/Program.cs
+++ PWork4/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            int firstChar = 'a', lastChar = 'z'; // range
+
+            string charArray = ""; // one-dimensional array of symbols
+
+            for (int i = firstChar; i <= lastChar; i++)
+            {
+                charArray += (char)i;
+            }
+
+            Console.WriteLine("\n\t\t    ASCII Table (a-z)\n");
+            foreach (char c in charArray)
+            {
+                Console.Write($"\'{c}\' - ASCII: {(int)c:D3}  | ");
+                if (c % 3 == 0)
+                    Console.WriteLine("\n-----------------------------------------------------------");
+            }
 
 
             Console.ReadKey();
