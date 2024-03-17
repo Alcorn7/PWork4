68d9b860ab92bcccee386cfe00165166e8500ae1
Andrii99H <andrii99havryliuk@gmail.com>
2024-03-17 13:47:51 +0200

    Initial task3

--- PWork4/Program.cs
+++ PWork4/Program.cs
@@ -1,16 +1,12 @@
 ﻿namespace PWork4
 {
-    internal class Task1
+    internal class Task3
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[50]; 
             
-            for (int i = 1; i <= numbers.Length; i++)
-                numbers[i-1] = i * 2;
 
-            foreach (int num in numbers)
-                Console.WriteLine($"Element: {num}");
+
 
             Console.ReadKey();
         }
